37603b607f9dc74b33bf6f5d12d035a2ffa8fc88
Tyler Rhodes <rhodetyl000@gmail.com>
2021-02-28 22:09:45 -0600

    Add update action to controller and routes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,4 +10,14 @@ class MoviesController < ApplicationController
   def edit
     @movie = Movie.find(params[:id])
   end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update(movie_params)
+    redirect_to @movie
+  end
+
+  def movie_params
+    params.require("movie").permit(:title, :description, :rating, :released_on, :total_gross)
+  end
 end
