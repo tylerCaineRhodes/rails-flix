f96dd96922ca7d2d265fb4ab3981143066db7519
Tyler Rhodes <rhodetyl000@gmail.com>
2021-02-28 23:37:23 -0600

    Enable deletion of movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -26,10 +26,16 @@ class MoviesController < ApplicationController
     @movie.save
     redirect_to @movie
   end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path
+  end
 end
 
 private
 
-  def movie_params
-    params.require("movie").permit(:title, :description, :rating, :released_on, :total_gross)
-  end
+def movie_params
+  params.require("movie").permit(:title, :description, :rating, :released_on, :total_gross)
+end
