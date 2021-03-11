c0671ac99b5bfc7d9a209ebf1f6403cece0f5331
Tyler Rhodes <rhodetyl000@gmail.com>
2021-03-10 21:32:45 -0800

    Remove redundant variable declarations

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,17 +2,14 @@ class ReviewsController < ApplicationController
   before_action :set_movie
 
   def index
-    @movie = Movie.find(params[:movie_id])
     @reviews = @movie.reviews
   end
 
   def new
-    @movie = Movie.find(params[:movie_id])
     @review = @movie.reviews.new
   end
 
   def create
-    @movie = Movie.find(params[:movie_id])
     @review = @movie.reviews.new(review_params)
     if @review.save
       redirect_to movie_reviews_path(@movie), notice: "Thanks for your review!"
@@ -21,7 +18,6 @@ class ReviewsController < ApplicationController
       render 'new'
     end
   end
-
 end
 
 private
