e2afd3538044930ffd2d42f5675e692c01677732
Tyler Rhodes <rhodetyl000@gmail.com>
2021-03-10 21:30:43 -0800

    Display the average review for a movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -32,4 +32,12 @@ class Movie < ApplicationRecord
     total_gross < 25000000
   end
 
+  def average_stars
+    reviews.average(:stars) || 0.0
+  end
+
+  def average_stars_as_percent
+    (self.average_stars / 5.0) * 100
+  end
+
 end
