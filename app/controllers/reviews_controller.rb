ac6df106c76c2aaeadf2724c124c77b7751cdbcc
Tyler Rhodes <rhodetyl000@gmail.com>
2021-03-10 20:53:55 -0800

    Allow creation of reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,36 @@
 class ReviewsController < ApplicationController
-  def index 
+  before_action :set_movie
+
+  def index
     @movie = Movie.find(params[:movie_id])
     @reviews = @movie.reviews
   end
+
+  def new
+    @movie = Movie.find(params[:movie_id])
+    @review = @movie.reviews.new
+  end
+
+  def create
+    @movie = Movie.find(params[:movie_id])
+    @review = @movie.reviews.new(review_params)
+    if @review.save
+      redirect_to movie_reviews_path(@movie), notice: "Thanks for your review!"
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+
 end
+
+private
+
+def review_params
+  params.require(:review).permit(:name, :stars, :comment)
+end
+
+def set_movie
+  @movie = Movie.find(params[:movie_id])
+end
+
