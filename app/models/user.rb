396aaee7077d3093a84c784fd049c37b41400b56
Tyler Rhodes <rhodetyl000@gmail.com>
2021-03-13 10:23:37 -0800

    Add resource for users

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+ class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, presence: true
+  validates :email, format: { with: /\S+@\S+/ },
+   uniqueness: { case_sensitive: false }
+  
+  
+end
