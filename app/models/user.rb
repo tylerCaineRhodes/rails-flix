802e92ce109d76138e9308d6daebf4aff1e74719
Tyler Rhodes <rhodetyl000@gmail.com>
2021-03-13 18:11:33 -0800

    Add profile image to user page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@
 
   validates :name, presence: true
   validates :email, format: { with: /\S+@\S+/ },
-   uniqueness: { case_sensitive: false }
+    uniqueness: { case_sensitive: false }
   
-  
+  def gravatar_id
+    Digest::MD5::hexdigest(email.downcase)
+  end
 end
