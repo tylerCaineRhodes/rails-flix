824108f8d64d1bd4b2e7a05575d962c3bf38b5ca
Tyler Rhodes <rhodetyl000@gmail.com>
2021-03-13 19:15:28 -0800

    Allow user to update and delete account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,8 +19,26 @@ class UsersController < ApplicationController
       render :new
     end
   end
-end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: "Account Successfully Updated!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to movies_url, alert: "Account Succesfully deleted!"
+  end
+end
 
 private 
 
