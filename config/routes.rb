c52810f0b615c61f175605b0ae18ea109301534a
Tyler Rhodes <rhodetyl000@gmail.com>
2021-03-17 21:37:00 -0700

    Add user sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   root "movies#index"
+  get "signup" => "users#new"
+  get "signin" => "sessions#new"
 
   resources :users
-  get "signup" => "users#new"
 
   resources :movies do 
     resources :reviews
   end
+
+  resource :session, only: [:new, :create, :destroy]
 end
  
