eb93d5da22904586b2fd04389db5acac73c063dd
Tyler Rhodes <rhodetyl000@gmail.com>
2021-03-17 21:37:00 -0700

    Add user sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email_or_username]) ||
+     User.find_by(username: params[:email_or_username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user[:id]
+      redirect_to user, notice: "Welcome back, #{user.name}!"
+    else
+      flash.now[:alert] = "Invalid email/password combination!"
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to movies_url, notice: "You've signed out!"
+  end
+end
