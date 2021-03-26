e4e21601818cba567009623ad7537a4a38290f2c
Tyler Rhodes <rhodetyl000@gmail.com>
2021-03-25 20:20:14 -0700

    Add favorites resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
  class User < ApplicationRecord
   has_secure_password
   has_many :reviews, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   validates :name, presence: true
   validates :username, presence: true, format: { with: /\A[A-Z0-9]+\z/i }, uniqueness: { case_sensitive: false }
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
   root "movies#index"
   get "signup" => "users#new"
   get "signin" => "sessions#new"
-
+  
   resources :users
-
+  
   resources :movies do 
     resources :reviews
   end
+  
+  resources :favorites
 
   resource :session, only: [:new, :create, :destroy]
 end
